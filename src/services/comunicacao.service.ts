61e6cfdd318379d07b57aec20eafa096825be80a
Giovana Rocha <giovanarocha109@gmail.com>
2024-11-23 23:34:12 -0300

    Telas - adição do sistema solar fase dificl - Giovana

--- src/services/comunicacao.service.ts
+++ src/services/comunicacao.service.ts
@@ -9,20 +9,20 @@ export class ComunicacaoService {
 
   private dialog = inject(MatDialog);
 
-  // Declarando o tipo correto para dialogManutencaoRef
   private dialogManutencaoRef: MatDialogRef<ManutencaoComponent> | null = null;
 
-  // Função que abre o modal de manutenção e o fecha após 3 segundos
+
   avisoManutencao(): void {
-    // Abre o modal e atribui o valor à variável dialogManutencaoRef
     this.dialogManutencaoRef = this.dialog.open(ManutencaoComponent);
     
     setTimeout(() => {
-      // Verifica se o modal está aberto e fecha após 3 segundos
       if (this.dialogManutencaoRef) {
         this.dialogManutencaoRef.close();
       }
     }, 3000);
   }
+
+ 
+
 
 }
